0da4a8052cc1e260c864589d87bf193d82abf7ea
Darrell Tunnell <darrell.tunnell@reach-contact.com>
2017-11-09 13:18:07 +0000

    Repro

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Library;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new ServiceCollection();         
+
+            var startup = new Startup();
+            var serviceProvider = startup.Build(collection);        
+
+        }
+    }
+}
---
+++ Library/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Library
+{  
+    public class Startup 
+    {
+        public IServiceProvider Build(IServiceCollection services)
+        {
+            return services.BuildServiceProvider();
+        }       
+    }
+}
